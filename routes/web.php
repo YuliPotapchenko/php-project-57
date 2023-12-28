4572a3057ba3884299f363998cfa81906e2bfe83
julia <YuliPotapchenko@yandex.ru>
2023-12-28 09:24:01 +0300

    steps

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\{
     TaskStatusController,
     TaskController,
@@ -21,6 +22,8 @@ use App\Http\Controllers\{
 Route::get('/', function () {
     return view('index');
 })->name('index');
+
+Auth::routes();
 
 Route::resources([
     'task_statuses' => TaskStatusController::class,
