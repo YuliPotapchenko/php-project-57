6da1007ec96bbc65a6e00fa1b7d86bf758d6214e
Yulia Potapchenko <Yulia.Potapchenko@miningelement.com>
2024-01-15 15:38:40 +0300

    check 3

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -62,7 +62,7 @@ class TaskPolicy
      */
     public function delete(User $user, Task $task)
     {
-        return $user->id === $task->user_id;
+        return $user->id === $task->created_by_id;
     }
 
     /**
